5a5d874fa92057b197e724d684baeadac5f3c036
Philipp Middendorf <middendorf@plapadoo.de>
2017-12-01 13:19:44 +0100

    Revert "Try hSetEncoding utf8 as first main statement"
    This reverts commit a00139a41f441e3c5c63c1ea63f351b30a66707b.

--- app/Main.hs
+++ app/Main.hs
@@ -29,8 +29,7 @@ import           Plpd.Http                     (MonadHttp (..), hrContent,
 import           Plpd.MonadLog                 (MonadLog (..), defaultLog)
 import           Plpd.Util                     (textShow)
 import           Prelude                       (error)
-import           System.IO                     (IO, hSetEncoding, stderr,
-                                                stdout, utf8)
+import           System.IO                     (IO)
 import           System.Random                 (randomIO)
 import           Web.Matrix.API                (MatrixContext (..),
                                                 MatrixLoginRequest (..),
@@ -78,8 +77,6 @@ instance MonadMatrix MyMonad where
 
 main :: IO ()
 main = do
-    hSetEncoding stdout utf8
-    hSetEncoding stderr utf8
     options <- readProgramOptions
     configOptions <- readConfigOptions (options ^. poConfigFile)
     mvar <- newMVar ()
