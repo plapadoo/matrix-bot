ec1c1503ff79d1491dfcdd5c8dd94d2f4e9662e6
Philipp Middendorf <middendorf@plapadoo.de>
2018-02-28 18:39:37 +0100

    default.nix: Better shell behavior

--- default.nix
+++ default.nix
@@ -1,3 +1,5 @@
 { pkgs ? import <nixpkgs> {} }:
-
-pkgs.haskellPackages.callCabal2nix "matrix-bot" ./. {}
+let
+  drv = pkgs.haskellPackages.callCabal2nix "matrix-bot" ./. {};
+in
+  if pkgs.lib.inNixShell then drv.env else drv
